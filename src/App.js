7409f2ac365341cba83dccb80bb09396df8d958a
razvanr91 <razvan.roman91@outlook.com>
2021-11-18 13:12:03 +0000

    Add items to lists dynamic

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
+import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Expenses from './components/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const INITIAL_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Water',
+    amount: 13.22,
+    date: new Date(2021, 6, 11)
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 640.99,
+    date: new Date(2020, 2, 12)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 62.44,
+    date: new Date(2021, 11, 26)
+  },
+  {
+    id: 'e4',
+    title: 'New Laptop',
+    amount: 1500,
+    date: new Date(2021, 10, 31)
+  }
+];
+
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Water',
-      amount: 13.22,
-      date: new Date(2021, 6, 11)
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 640.99,
-      date: new Date(2020, 2, 12)
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 62.44,
-      date: new Date(2021, 11, 26)
-    },
-    {
-      id: 'e4',
-      title: 'New Laptop',
-      amount: 1500,
-      date: new Date(2021, 10, 31)
-    }
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+
+
 
   function addNewExpenseHandler(expense) {
-    expenses.push(expense);
+    setExpenses(previousExpenses => {
+      return [expense, ...previousExpenses];
+    });
   }
 
   return (
